1ae9f88514a3a9c2cebeb225117abf9dedc2a4c9
subrata-bhunia <eerrcfgy@gmail.com>
2022-12-04 13:17:05 +0530

    first commit

---
+++ SumOfOddEven/Program.cs
@@ -0,0 +1,41 @@
+﻿// Ex: 5.1 (i)
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfOddEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 8;
+            int[] numArr = new int[num];
+            int sumOfOdd = 0;
+            int sumOfEven = 0;
+            for(int i = 0; i < num; i++)
+            {
+                numArr[i] = Convert.ToInt32(Console.ReadLine()); 
+            }
+            for (int j = 0; j < num; j++)
+            {
+                if(numArr[j] % 2 == 0)
+                {
+                    sumOfEven += numArr[j];
+                }
+                else
+                {
+                    sumOfOdd += numArr[j];
+
+                }
+            }
+            Console.WriteLine("Sum of given Odd Numbers is {0}",sumOfOdd);
+            Console.WriteLine("Sum of given Even Numbers is {0}", sumOfEven);
+        }
+    }
+}
